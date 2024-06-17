d77f41261eda1c117a93768c60b3f751118e3fd2
SeokHyeong6320 <hsh9171@icloud.com>
2024-06-17 22:06:01 +0900

    initial

---
+++ src/test/java/com/health/dietservice/DietServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.dietservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

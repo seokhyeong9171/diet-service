c1afc625d7d752cb348d26cb59eaccb5c4e8c7d5
SeokHyeong6320 <hsh9171@icloud.com>
2024-07-04 15:11:57 +0900

    feature: redis 설정

--- common/src/main/java/com/health/common/redis/RedisKeyComponent.java
+++ common/src/main/java/com/health/common/redis/RedisKeyComponent.java
@@ -6,8 +6,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class RedisKeyComponent {
 
-  public String intakeKey(String authId, LocalDate date) {
+  // 유저의 권장 칼로리, 해당 일 섭취 칼로리 조회
+  public static String intakeKey(String authId, LocalDate date) {
     return "intake:" + date + ":" + authId;
+  }
+
+  // 게시글 좋아요 수
+  public static String postLikeValue() {
+    return "post:like";
+  }
+
+  // 유저 별 좋아요 누른 게시글
+  public static String userPostLike(String authId) {
+    return "user:" + authId + "post:like";
   }
 
 }
---
+++ forum-service/src/main/java/com/health/forumservice/config/ForumRedisConfig.java
@@ -0,0 +1,20 @@
+package com.health.forumservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class ForumRedisConfig {
+
+  @Bean
+  public RedisTemplate<String, String> StringRedisTemplate() {
+    RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+    return redisTemplate;
+  }
+
+}

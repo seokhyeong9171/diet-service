d41bd41b43c32b3ccb7ea24d5d0385a9fc4e12ea
SeokHyeong6320 <hsh9171@icloud.com>
2024-07-07 13:34:32 +0900

    feature: CommentController

---
+++ api/src/main/java/com/health/api/controller/CommentController.java
@@ -0,0 +1,143 @@
+package com.health.api.controller;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+import com.health.api.form.CommentForm;
+import com.health.api.service.CommentApplication;
+import com.health.common.model.SuccessResponse;
+import com.health.domain.dto.CommentDomainDto;
+import com.health.domain.response.CommentListResponse;
+import com.health.domain.response.CommentListResponse.CommentResponse;
+import com.health.security.authentication.AuthValidatorComponent;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/forum/posts/{postId}/comments")
+@RequiredArgsConstructor
+public class CommentController {
+
+  private final AuthValidatorComponent authValidatorComponent;
+  private final CommentApplication commentApplication;
+
+  @GetMapping
+  public ResponseEntity<?> getCommentList(
+      @CookieValue("Authorization") String jwt, @PathVariable Long postId
+  ) {
+    authValidatorComponent.validateAuthId(jwt);
+
+    List<CommentDomainDto> commentDomainDtoList = commentApplication.getCommentList(postId);
+
+    return ResponseEntity.ok(
+        SuccessResponse.of(commentDomainDtoList.stream().map(CommentResponse::fromDomainDto))
+    );
+  }
+
+  @PostMapping
+  public ResponseEntity<?> createComment(
+      @CookieValue("Authorization") String jwt, @PathVariable Long postId,
+      @RequestBody @Validated CommentForm commentForm
+  ) {
+    String authId = authValidatorComponent.validateAuthId(jwt);
+
+    CommentDomainDto commentDomainDto =
+        commentApplication.createComment(authId, postId, commentForm);
+
+    return ResponseEntity.status(CREATED).body(
+        SuccessResponse.of(CommentResponse.fromDomainDto(commentDomainDto))
+    );
+  }
+
+  @PatchMapping("/{commentId}")
+  public ResponseEntity<?> updateComment(
+      @CookieValue("Authorization") String jwt,
+      @PathVariable Long postId, @PathVariable Long commentId,
+      @RequestBody @Validated CommentForm commentForm
+  ) {
+    String authId = authValidatorComponent.validateAuthId(jwt);
+
+    CommentDomainDto commentDomainDto =
+        commentApplication.updateComment(authId, postId, commentId, commentForm);
+
+    return ResponseEntity.status(CREATED).body(
+        SuccessResponse.of(CommentResponse.fromDomainDto(commentDomainDto))
+    );
+  }
+
+  @DeleteMapping("/{commentId}")
+  public ResponseEntity<?> deleteComment(
+      @CookieValue("Authorization") String jwt,
+      @PathVariable Long postId, @PathVariable Long commentId
+  ) {
+
+    String authId = authValidatorComponent.validateAuthId(jwt);
+
+    Long deletedCommentId = commentApplication.deleteComment(authId, postId, commentId);
+
+    return ResponseEntity.ok(SuccessResponse.of("deleted commentId: " + deletedCommentId));
+  }
+
+
+
+  @PostMapping("/{commentId}")
+  public ResponseEntity<?> createChildComment(
+      @CookieValue("Authorization") String jwt,
+      @PathVariable Long postId, @PathVariable Long commentId,
+      @RequestBody @Validated CommentForm commentForm
+  ) {
+    String authId = authValidatorComponent.validateAuthId(jwt);
+
+    CommentDomainDto commentDomainDto =
+        commentApplication.createSubComment(authId, postId, commentId, commentForm);
+
+    return ResponseEntity.status(CREATED).body(
+        SuccessResponse.of(CommentListResponse.fromDomainDto(commentDomainDto))
+    );
+  }
+
+  @PatchMapping("/{commentId}/{childCommentId}")
+  public ResponseEntity<?> updateChildComment(
+      @CookieValue("Authorization") String jwt,
+      @PathVariable Long postId, @PathVariable Long commentId, @PathVariable Long childCommentId,
+      @RequestBody @Validated CommentForm commentForm
+  ) {
+
+    String authId = authValidatorComponent.validateAuthId(jwt);
+
+    CommentDomainDto commentDomainDto =
+        commentApplication.updateChildComment(authId, postId, commentId, childCommentId, commentForm);
+
+    return ResponseEntity.status(CREATED).body(
+        SuccessResponse.of(CommentResponse.fromDomainDto(commentDomainDto))
+    );
+  }
+
+  @DeleteMapping("/{commentId}/{childCommentId}")
+  public ResponseEntity<?> deleteChildComment(
+      @CookieValue("Authorization") String jwt,
+      @PathVariable Long postId, @PathVariable Long commentId, @PathVariable Long childCommentId
+  ) {
+
+    String authId = authValidatorComponent.validateAuthId(jwt);
+
+    Long deletedChildCommentId =
+        commentApplication.deleteChildComment(authId, postId, commentId, childCommentId);
+
+    return ResponseEntity.ok(SuccessResponse.of("deleted commentId: " + deletedChildCommentId));
+  }
+
+
+
+
+}

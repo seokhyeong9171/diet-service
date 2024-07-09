309b689d5848f05610fee17c201ccebebdfe815e
SeokHyeong6320 <hsh9171@icloud.com>
2024-07-09 19:08:02 +0900

    refactor: 리팩토링

--- domain/src/main/java/com/health/domain/entity/PostLikeEntity.java
+++ domain/src/main/java/com/health/domain/entity/PostLikeEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -20,7 +21,7 @@ import lombok.NoArgsConstructor;
 @Getter @Builder
 public class PostLikeEntity {
 
-  @Id @GeneratedValue
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "post_like_id")
   private Long id;
 
--- domain/src/main/java/com/health/domain/entity/PostViewEntity.java
+++ domain/src/main/java/com/health/domain/entity/PostViewEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -21,7 +22,7 @@ import lombok.NoArgsConstructor;
 @Getter @Builder
 public class PostViewEntity {
 
-  @Id @GeneratedValue
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "post_view_id")
   private Long id;
 

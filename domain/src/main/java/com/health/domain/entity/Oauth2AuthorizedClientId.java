b89c97ad20c90d57735d3d9810de41ea16248208
SeokHyeong6320 <hsh9171@icloud.com>
2024-07-12 10:13:56 +0900

    feature: Oauth2AuthorizationClient entity 추가

---
+++ domain/src/main/java/com/health/domain/entity/Oauth2AuthorizedClientId.java
@@ -0,0 +1,41 @@
+package com.health.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+@Getter
+@Setter
+@Embeddable
+public class Oauth2AuthorizedClientId implements java.io.Serializable {
+
+  private static final long serialVersionUID = -2194764910603450861L;
+
+  @Column(name = "client_registration_id", nullable = false, length = 100)
+  private String clientRegistrationId;
+
+  @Column(name = "principal_name", nullable = false, length = 200)
+  private String principalName;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+      return false;
+    }
+    Oauth2AuthorizedClientId entity = (Oauth2AuthorizedClientId) o;
+    return Objects.equals(this.clientRegistrationId, entity.clientRegistrationId) &&
+        Objects.equals(this.principalName, entity.principalName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(clientRegistrationId, principalName);
+  }
+
+}
